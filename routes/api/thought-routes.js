b211c64116fcf5a4251f0a7e353a06621b30d2ee
Ian Linton <ilinton2571@gmail.com>
2022-03-14 15:33:30 -0700

    thought route

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,21 @@
 const router = require('express').Router();
+const {
+    addThought,
+    removeThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller')
 
+// /api/thoughts
+router.route('/:thoughtId')
+
+router
+    .route('/:thoughtId')
+    .put(addReaction)
+    .delete(removeThought)
+
+router
+    .route(':/thoughtId/:reactionId')
+    .delete(removeReaction)
+    
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nosql-challenge', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/nosql-challenge', {
     // useFindAndModify: false,
     // useNewURLParser: true,
     // useUnifiedTopology: true
