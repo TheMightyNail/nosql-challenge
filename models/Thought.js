7989dc9b62970911c6690eea7b15c98731775b6d
Ian Linton <ilinton2571@gmail.com>
2022-03-14 14:03:24 -0700

    add thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,32 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ThoughtSchema = new Schema(
+    {
+        thoughtId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        thoughtText: {
+            type: String, 
+            required: function() { return this.thoughtId != null; },
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: function() { return this.userId != null; }
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
