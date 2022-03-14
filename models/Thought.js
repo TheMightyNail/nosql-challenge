a4911a37409359a4d8642bbe26c8870bd58510f5
Ian Linton <ilinton2571@gmail.com>
2022-03-14 12:42:19 -0700

    add User model

--- models/Thought.js
+++ models/Thought.js
@@ -1,16 +1,3 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const UserSchema = new Schema(
-    {
-        userID: {
-            type: Schema.Types.ObjectId,
-            default: () => Types.ObjectId()
-        },
-        username: {
-            type: String, 
-            
-        }
-        
-    }
-)
---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        userID: {
+            type: Schema.Types.ObjectId,
+            default: () => Types.ObjectId()
+        },
+        username: {
+            type: String, 
+            required: function() { return this.userId != null; },
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: function() { return this.userId != null; },
+            unique: true,
+            validate: [validateEmail, 'Please enter a valid e-mail address'],
+            match: ['/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/', 'Please enter a valid e-mail address']
+        },
+        // thoughts: {
+            // array matching { _id } for thoughts model 
+        // },
+        // friends: {
+            // array { _id } self referencing User Model ?
+        // }
+        
+    }
+)
